66e46693bf5f1cb5d919fbe837abf4f710d535ee
Himanshu-Khati <himanshu.khati@edgecx.com>
2025-04-09 12:29:17 +0530

    hk: base url fix

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://qrcoder-server.onrender.com/api/v1";
-const API_BASE_URL = "http://localhost:4000/api/v1";
+const API_BASE_URL = "https://qrcoder-server.onrender.com/api/v1";
+// const API_BASE_URL = "http://localhost:4000/api/v1";
 export const API_ENDPOINTS = {
   SEND_OTP: `${API_BASE_URL}/auth/sendOtp`,
   REGISTER: `${API_BASE_URL}/auth/register`,
